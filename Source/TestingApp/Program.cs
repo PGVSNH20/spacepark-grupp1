99c3fe63fdefdd711959cf6504535ae8cc32b202
Edgar Runnman <edgar.runnman@gmail.com>
2021-03-18 16:16:05 +0100

    commit

---
+++ Source/TestingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace TestingApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest("people/", DataFormat.Json);
+            // NOTE: The Swreponse is a custom class which represents the data returned by the API, RestClient have buildin ORM which maps the data from the reponse into a given type of object
+            var peopleResponse = client.Get<Users>(request);
+
+            //foreach(var user in )
+            //Console.WriteLine(peopleResponse.Data.name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Source/TestingApp/user.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingApp
+{
+    internal class User
+    {
+        public string name { get; set; }
+    }
+
+    internal class Users
+    {
+        //[attribute mappning]
+        public List<User> users { get; set; }
+    }
+}
