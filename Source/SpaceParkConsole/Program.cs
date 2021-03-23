50da8642fd531d67aee8f919f8635c1a85dc48f2
Elsa Matteoni <elsa.matteoni@hotmail.com>
2021-03-23 15:26:54 +0100

    Commit

--- Source/SpaceParkConsole/Program.cs
+++ Source/SpaceParkConsole/Program.cs
@@ -7,8 +7,25 @@ namespace SpaceParkConsole
 {
     internal class Program
     {
+        
         private static async Task Main(string[] args)
         {
+            Console.WriteLine("Welcome to SpacePark, please enter your name:");
+            string name = Console.ReadLine();
+
+            //Ange om det finns en pågående parkering
+
+            //Ge val: starta ny/avsluta pågående/ändra pågående
+
+            //Starta: ange spaceship, visa lediga platser, ange sluttid
+            //Meddela: parkering startad, position, förväntad kostnad
+
+            //Avsluta: ange spaceship
+            //Meddela: slutpris, fakturaadress, betaldatum
+
+            //Ändra: ange spaceship, ny sluttid
+            //Meddela: parkering startad, position, förväntad kostnad
+
             var swapi = new SWApi();
             //var usersList = swapi.GetAllStarShips();
             var user = await swapi.GetUserById(1);
